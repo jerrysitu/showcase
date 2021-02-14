3d24599f7b885a3f89f72f037e136ceb94eeee13
JerryS <jerry@jerrysitu.com>
2021-02-14 13:42:00 -0800

    blah

--- config/prod.exs
+++ config/prod.exs
@@ -17,10 +17,10 @@ config :showcase, ShowcaseWeb.Endpoint,
   ],
   # server: true,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  # check_origin: [
-  #   "https://#{System.get_env("ORIGIN_URL")}",
-  #   "https://#{System.get_env("RENDER_EXTERNAL_HOSTNAME")}"
-  # ],
+  check_origin: [
+    "https://#{System.get_env("ORIGIN_URL")}",
+    "https://#{System.get_env("RENDER_EXTERNAL_HOSTNAME")}"
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
