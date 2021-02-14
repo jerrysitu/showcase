db1b2114018407ee2c232e5ae42881e8d55a1a6c
JerryS <jerry@jerrysitu.com>
2021-02-14 14:18:31 -0800

    blah

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :showcase, ShowcaseWeb.Endpoint,
     port: 80
   ],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  # server: true,
+  server: true,
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   check_origin: [
     "https://#{System.get_env("ORIGIN_URL", "")}",
@@ -25,6 +25,7 @@ config :showcase, ShowcaseWeb.Endpoint,
     "https://jerrysitu.com",
     "//jerrysitu.com"
   ],
+  code_reloader: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
