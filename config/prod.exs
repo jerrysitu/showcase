1178b351ffcb53efc844c3774afe45e4756b7c96
JerryS <jerry@jerrysitu.com>
2021-02-14 13:29:21 -0800

    blah

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :showcase, ShowcaseWeb.Endpoint,
-  url: [host: System.get_env("ORIGIN_HOSTNAME") || "localhost", port: 80],
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   server: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  check_origin: ["https://#{System.get_env("ORIGIN_HOSTNAME")}"],
+  check_origin: [
+    "https://#{System.get_env("ORIGIN_URL")}",
+    "https://#{System.get_env("RENDER_EXTERNAL_HOSTNAME")}"
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
