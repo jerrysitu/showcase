e9d36824aac2401cbd2369d461ed0fa11ddf1ec7
Jerry S <jerry@jerrysitu.com>
2021-12-11 12:58:28 -0800

    change build script

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ set -o errexit
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
-
+MIX_ENV=prod mix assets.deploy
 # Compile assets
-npm install --prefix ./assets
-npm run deploy --prefix ./assets
-mix phx.digest
+
+# npm install --prefix ./assets
+# npm run deploy --prefix ./assets
+# mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
