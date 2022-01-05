74e284c0faa06b4b21e85bac246257b4c8325fd2
Jerry S <jerry@jerrysitu.com>
2022-01-04 18:41:43 -0800

    put a @

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,6 +3,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("tailwindcss-neumorphism"),
+    require("@tailwindcss-neumorphism"),
   ],
 };
