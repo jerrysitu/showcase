f1e702d26309d568934cfa4bed7353f7e19fb7df
Jerry S <jerry@jerrysitu.com>
2022-01-04 18:31:33 -0800

    update buld.sh add node

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ MIX_ENV=prod mix compile
 MIX_ENV=prod mix assets.deploy
 # Compile assets
 
-# npm install --prefix ./assets
-# npm run deploy --prefix ./assets
-# mix phx.digest
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
+mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
